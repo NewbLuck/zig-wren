3b2dfc9ec2d5bd8cf2ac15f9540ae3c4d072e7e2
NewbLuck <tehscott@gmail.com>
2021-08-04 02:41:31 -0600

    Update example_build.zig

--- example/example_build.zig
+++ example/example_build.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const wrenBuild = @import("deps/wren/build.zig");
+const wrenBuild = @import("deps/zig-wren/build.zig");
 
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
